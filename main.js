3f46f05b2c9f7bff46099c9721dc2fce5509f968
Andrea Hohnsen <a.hohnsen@mail.de>
2022-01-26 16:58:46 +0100

    Change text on answer-button with .textContent on bookmarks.html

--- main.js
+++ main.js
@@ -19,8 +19,9 @@ const answerText = document.querySelector('[data-js="answer-text"]');
 answerButton.addEventListener('click', () => {
   if (answerButton.textContent === 'Show answer') {
     answerButton.textContent = 'Hide answer';
-    answerText.classList.toggle('card__answer--hide');
+    answerText.classList.remove('card__answer--hide');
   } else {
     answerButton.textContent = 'Show answer';
+    answerText.classList.add('card__answer--hide');
   }
 });
