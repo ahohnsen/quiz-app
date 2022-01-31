8a7e8e02c1a682246eecda0a8b74c04e6cc4a14c
Andrea Hohnsen <a.hohnsen@mail.de>
2022-01-31 18:16:16 +0100

    Update navigation.js

--- js/Navigation.js
+++ js/Navigation.js
@@ -1,22 +1,26 @@
-export default function setupNavigation() {
-  document.querySelectorAll('.navigation__button').forEach(button => {
-    button.addEventListener('click', () => {
-      const navigation = button.parentElement;
-      const pageContainer = navigation.parentElement;
-      const pageNumber = button.dataset.nav;
-      const pageActive = pageContainer.querySelector(
-        `.pages[data-pages="${pageNumber}"]`
-      );
-      navigation.querySelectorAll('.navigation__button').forEach(button => {
+export default function Navigation() {
+  const allButtons = document.querySelectorAll('[data-nav]');
+  const allPages = document.querySelectorAll('[data-page]');
+  const header = document.querySelector('[data-header]');
+
+  allButtons.forEach(button => {
+    button.addEventListener('click', event => {
+      allButtons.forEach(button => {
         button.classList.remove('navigation__button--active');
       });
-
-      pageContainer.querySelectorAll('.pages').forEach(page => {
-        page.classList.remove('pages--active');
-      });
-
       button.classList.add('navigation__button--active');
-      pageActive.classList.add('pages--active');
+
+      allPages.forEach(page => {
+        page.classList.add('hidden');
+      });
+      const buttonAttribute = event.currentTarget.getAttribute('data-nav');
+      const currentPage = document.querySelector(
+        `[data-page="${buttonAttribute}"]`
+      );
+      currentPage.classList.remove('hidden');
+      const capitalHeader =
+        buttonAttribute.charAt(0).toUpperCase() + buttonAttribute.slice(1);
+      header.textContent = `Quiz App | ${capitalHeader}`;
     });
   });
 }
